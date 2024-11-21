c8479c2a6e9b8066e87f7b568edc4af5f088660b
Kaitlin Isaac <kmi14505@gmail.com>
2024-11-21 15:21:08 -0500

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Glacial Indifference"', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'section-blog': '#E5F4F7',
+        'section-talks': '#F0F7E6',
+        'accent-yellow': '#FFE600',
+        'menu-1': '#28BCCB',
+        'menu-2': '#87CB28',
+        'menu-3': '#FBAAFF',
+      },
+    },
+  },
+  plugins: [],
+};
